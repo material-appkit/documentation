e279a240a1644ba80060e8389e93b0ea99cd2e90
Allan Hart <allan.w.hart@gmail.com>
2020-12-30 18:00:24 -0700

    Introduced "defaults.json"

--- plugins/gatsby-plugin-no-ie/gatsby-browser.js
+++ plugins/gatsby-plugin-no-ie/gatsby-browser.js
@@ -1,8 +1,8 @@
-const DEFAULT_PATH = '/noie';
+const defaults = require('./defaults');
 
 export const onClientEntry = (_, options) => {
-  const isIE = false || !!document.documentMode;
-  const pathname = options ? (options.pathname || DEFAULT_PATH) : DEFAULT_PATH;
+  const isIE = true || !!document.documentMode;
+  const pathname = options ? (options.pathname || defaults.pathname) : defaults.pathname;
 
   if (isIE && window.location.pathname !== pathname) {
     window.location = pathname;
--- plugins/gatsby-plugin-no-ie/gatsby-node.js
+++ plugins/gatsby-plugin-no-ie/gatsby-node.js
@@ -1,13 +1,12 @@
+const defaults = require('./defaults');
 const path = require('path');
-
-const DEFAULT_PATH = '/noie';
 
 exports.createPages = (args, options) => {
   const { actions } = args;
   const { createPage } = actions;
 
   createPage({
-    path: options ? (options.pathname || DEFAULT_PATH) : DEFAULT_PATH,
+    path: options ? (options.pathname || defaults.pathname) : defaults.pathname,
     component: path.resolve(`${__dirname}/src/no-ie.js`),
   });
 };
