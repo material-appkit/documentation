d7bd7fbde1527c3816a7294026544eb724b472a1
allan <allan.w.hart@gmail.com>
2021-01-05 08:36:31 -0700

    CodeView uses font Cascadia Code

--- src/components/CodeView.js
+++ src/components/CodeView.js
@@ -1,29 +1,50 @@
+import clsx from 'clsx';
+
 import Prism from 'prismjs';
 
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles((theme) => ({
+  code: {
+    '&[class*="language-"]': {
+      fontFamily: "'Cascadia Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+    }
+  },
+
+  singleline: {
+    '&[class*="language-"]': {
+      lineHeight: 1,
+    }
+  },
+}));
 
 function CodeView({ children, language, singleline }) {
+  const classes = styles();
+
   const [html] = useState(() => {
     return {
       __html: Prism.highlight(children, Prism.languages[language], language)
     };
   });
 
-  const style = {
-    lineHeight: singleline ? 1 : 1.3,
-  };
+  const preClassNames = [`language-${language}`];
+  if (singleline) {
+    preClassNames.push(classes.singleline);
+  }
+
+  const codeClassNames = [`language-${language}`, classes.code];
+  if (singleline) {
+    codeClassNames.push(classes.singleline);
+  }
 
   return (
-    <pre
-      className={`language-${language}`}
-      style={style}
-    >
+    <pre className={clsx(preClassNames)}>
       <code
-        className={`language-${language}`}
+        className={clsx(codeClassNames)}
         dangerouslySetInnerHTML={html}
-        style={style}
       />
     </pre>
   );
